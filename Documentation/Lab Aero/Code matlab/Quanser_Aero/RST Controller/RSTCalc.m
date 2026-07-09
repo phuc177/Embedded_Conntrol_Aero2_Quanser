f96afeb92f56bc464c9e68f72fef3c13c08859e5
NGUYEN Van Hong Phuc (nguyevhp) <nguyevhp@esisar.inpg.fr>
2026-07-09 17:20:07 +0200

    - CHange script RST a bit

--- Documentation/Lab Aero/Code matlab/Quanser_Aero/RST Controller/RSTCalc.m
+++ Documentation/Lab Aero/Code matlab/Quanser_Aero/RST Controller/RSTCalc.m
@@ -12,11 +12,12 @@ B = K;
 
 %% Requirement Tset
 PeakTime = 1.5;
+Tset = 2;
 OS = 2.5;
 
 damping_r = -log(OS/100) / sqrt(pi^2 + log(OS/100)^2);
-omega_n = pi / (PeakTime*sqrt(1-damping_r^2));
-Tset = 4 / (damping_r*omega_n);
+omega_n = 4 / (damping_r*Tset);
+
 
 % coefficient of the wanted characteristic equation: d2*s^2 + d1*s + d0
 d2 = 1;
@@ -24,8 +25,8 @@ d1 = 2*damping_r*omega_n;
 d0 = omega_n^2;
 
 %% RST controller calculation
-p1 = -5;
-p2 = -7;
+p1 = -3;
+p2 = -3.5;
 CharEqn = (s^2 + d1*s + d0)*(s-p1)*(s-p2);
 % S : degree 2
 S = s*(s1*s + s0);
